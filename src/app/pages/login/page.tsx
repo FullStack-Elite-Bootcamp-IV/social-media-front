f9eda4c6ed54afaf115cfa5a9ee14f86f1ac239e
birulboy <brahian.castro@utp.edu.co>
2024-07-19 14:42:56 -0500

    login conflicts done

--- src/app/pages/login/page.tsx
+++ src/app/pages/login/page.tsx
@@ -1,56 +1,51 @@
-"use client";
+'use client'
 
 import { FormEvent, useEffect, useState } from "react";
 import { useAuth } from "../../context/authContext";
-import Cookies from "js-cookie";
+import Cookies from "js-cookie"
 
 const Login = () => {
-  const { loginToken, register, login, darkMode, handleDarkMode } = useAuth();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const { loginToken, register, login } = useAuth();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  useEffect(() => {
-    const token = Cookies.get("token");
-    if (token) {
-      const decodeToken = JSON.parse(token);
-    } else {
-      console.log("No hay cookie");
+
+    useEffect(() => {
+        const token = Cookies.get("token");
+        if (token) {
+            const decodeToken = JSON.parse(token);
+            console.log(decodeToken);
+        }
+        else {
+            console.log("No hay cookie");
+        }
+
+    }, [])
+
+    const handleLogin = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        login(email, password);
     }
-  }, [darkMode]);
 
-  const handleLogin = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    login(email, password);
-  };
 
-  const handleSetDarkMode = () => {
-    handleDarkMode();
-  };
 
-  return (
-    <main>
-      <form id="loginForm" onSubmit={handleLogin}>
-        <input
-          type="email"
-          id="email"
-          placeholder="Email"
-          required
-          onChange={(e) => setEmail(e.target.value)}
-        ></input>
-        <input
-          type="password"
-          id="password"
-          placeholder="Password"
-          required
-          onChange={(e) => setPassword(e.target.value)}
-        ></input>
-        <button type="submit">Login</button>
-      </form>
-      <h1> Login </h1>
-      <p> Roboto </p>
-      <button onClick={handleSetDarkMode}>modo vista</button>
-    </main>
-  );
-};
 
-export default Login;
+    return (
+        <main className="flex bg-darkVoid sm:bg-slateGray w-full min-h-screen">
+            <div  className="contenedor-login bg-darkVoid w-96 rounded-3xl pt-5 pb-5 pl-5 pr-5 m-auto">
+            <h1 className="text-center  text-blancoHueso text-4xl">nexo</h1>
+            <form id="loginForm" onSubmit={handleLogin} className="flex flex-col  mt-4 mx-4">
+                <label htmlFor="email" className="text-blancoHueso text-sm  mb-1">Email</label>
+                <input className="rounded-md bg-slateGray h-10" type="email" id="email" placeholder=" Email" required onChange={(e) => setEmail(e.target.value)}></input>
+                <label htmlFor="password" className="text-blancoHueso  text-sm  mb-1 mt-2">Password</label>
+                <input type="password" id="password" placeholder=" Password" required onChange={(e) => setPassword(e.target.value)} className="rounded-md bg-slateGray h-10"></input>
+                <button type="submit" className="text-blancoHueso bg-liquidLava rounded-md mt-2 h-10">Login</button>
+            </form>
+            <p className=" mt-5 text-center text-xs text-blancoHueso">Don’t have an account? <strong >Sign up</strong></p>
+            </div>
+            
+        </main>
+    )
+}
+
+export default Login;  
