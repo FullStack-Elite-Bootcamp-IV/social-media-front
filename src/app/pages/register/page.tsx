a3f7618206ad90c1427f372b3e07028e46093b80
MiguelCano-ia <129132845+MiguelCano-ia@users.noreply.github.com>
2024-07-19 15:09:01 -0500

    working on logic, desing and responsive ready

--- src/app/pages/register/page.tsx
+++ src/app/pages/register/page.tsx
@@ -16,11 +16,11 @@ const Register = () => {
         className="flex p-8 flex-col justify-center items-center gap-4 flex-1  rounded-[32px] bg-darkVoid
         max-w-[639px] max-sm:rounded-none max-sm:w-screen max-sm:h-screen "
       >
-        <h1 className="text-blancoHueso text-center text-5xl font-normal leading-[52px]">
+        <h1 className="text-blancoHueso text-center text-5xl font-normal leading-[52px] max-[320px]:text-3xl mt-5">
           Star
         </h1>
         <div className="flex flex-col justify-center items-center gap-2 self-stretch">
-          <p className="text-blancoHueso items-center text-sm font-normal leading-[27px]">
+          <p className="text-blancoHueso items-center text-sm font-normal leading-[27px] max-[320px]:text-sm">
             Sign up to see posts from your friends.
           </p>
         </div>
@@ -31,9 +31,9 @@ const Register = () => {
             </label>
             <input
               type="text"
-              className="flex h-14 p-4 items-center gap-1 self-stretch bg-slateGray"
+              className="flex h-14 p-4 items-center gap-1 self-stretch bg-slateGray  max-[320px]:h-10 text-blancoHueso"
               required
-              placeholder="Enter your username"
+              placeholder="eliteBootcamp"
             />
           </div>
           <div className="flex flex-col justify-center items-start gap-1 self-stretch">
@@ -41,10 +41,10 @@ const Register = () => {
               Email
             </label>
             <input
-              type="text"
-              className="flex h-14 p-4 items-center gap-1 self-stretch bg-slateGray"
+              type="email"
+              className="flex h-14 p-4 items-center gap-1 self-stretch bg-slateGray max-[320px]:h-10 text-blancoHueso"
               required
-              placeholder="Enter your email"
+              placeholder="elite@example.com"
             />
           </div>
           <div className="flex flex-col justify-center items-start gap-1 self-stretch">
@@ -52,12 +52,12 @@ const Register = () => {
               Password
             </label>
             <input
-              type="text"
-              className="flex h-14 p-4 items-center gap-1 self-stretch bg-slateGray"
+              type="password"
+              className="flex h-14 p-4 items-center gap-1 self-stretch bg-slateGray max-[320px]:h-10 text-blancoHueso"
               required
-              placeholder="Enter your password"
+              placeholder="isasecret"
             />
-            <button className="flex py-4 px-6 justify-center items-center gap-1 self-stretch rounded-lg bg-liquidLava mt-2">
+            <button className="flex py-4 px-6 justify-center items-center gap-1 self-stretch rounded-lg bg-liquidLava mt-2 hover:bg-ligthPurple transition-colors">
               <div className="flex h-[22px] px-1 justify-center items-center gap-2">
                 <p className="text-blancoHueso text-center text-md font-normal">
                   Sign up with Email
@@ -66,10 +66,12 @@ const Register = () => {
             </button>
           </div>
         </form>
-        <p className="text-center text-blancoHueso font-normal  text-sm">
-          Have and account?
-          <span className="text-blancoHueso font-extrabold">Log in</span>
-        </p>
+        <div>
+          <p className="text-center text-blancoHueso font-normal text-sm">
+            Have and account?
+            <span className="text-blancoHueso font-extrabold cursor-pointer"> Login</span>
+          </p>
+        </div>
       </section>
     </main>
   );
