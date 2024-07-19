f54324ba1f60c43298f616357d796d551fd85559
MiguelCano-ia <129132845+MiguelCano-ia@users.noreply.github.com>
2024-07-19 09:27:24 -0500

    Revert "forms without styles"
    This reverts commit f8e4e9cfefd65dbfb16fc98dba9005063d54e44a.

--- src/app/pages/profile/editprofile/page.tsx
+++ src/app/pages/profile/editprofile/page.tsx
@@ -1,73 +1,21 @@
-"use client";
+'use client'
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAuth } from "../../../context/authContext";
 
 const EditProfile = () => {
-  const { loginToken, register } = useAuth();
-  const [username, setUserName] = useState("");
-  const [fullname, setFullName] = useState("");
-  const [description, setDecription] = useState("");
-  const [profilePic, setProfilePic] = useState("");
-  const [website, setWebSite] = useState("");
-  const [gender, setGender] = useState("");
-  const [location, setLocation] = useState("");
+    const { loginToken, register } = useAuth();
 
-  const handleUpdate = () => {};
+    useEffect(() => {
+        console.log(loginToken);
+    }, []);
 
-  useEffect(() => {
-    console.log(loginToken);
-  }, []);
-
-  return (
-    <main className="bg-black">
-      <h1 className="bg-black">Update Information</h1>
-      <form id="UpdateForm" onSubmit={handleUpdate}>
-        <label>Username</label>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUserName(e.target.value)}
-        />
-        <label>Fullname</label>
-        <input
-          type="text"
-          placeholder="Fullname"
-          value={fullname}
-          onChange={(e) => setFullName(e.target.value)}
-        />
-        <label>Description</label>
-        <input
-          type="text"
-          placeholder="Description"
-          value={description}
-          onChange={(e) => setDecription(e.target.value)}
-        />
-        <label>Personal Website</label>
-        <input
-          type="text"
-          placeholder="Website"
-          value={website}
-          onChange={(e) => setWebSite(e.target.value)}
-        />
-        <label>Gender</label>
-        <input
-          type="text"
-          placeholder="Gender"
-          value={gender}
-          onChange={(e) => setGender(e.target.value)}
-        />
-        <label>Location</label>
-        <input
-          type="text"
-          placeholder="Location"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-        />
-      </form>
-    </main>
-  );
-};
+    return (
+        <main className="bg-black">
+            <h1>Edit profile</h1>
+            <p>Roboto</p>
+        </main>
+    );
+}
 
 export default EditProfile;
