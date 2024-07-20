3ad28925879887c803ccfb47a5b04288eea90da6
Joseph Martinez <insertepala@gmail.com>
2024-07-19 22:58:03 -0500

    Improved posts

--- src/app/components/post/Post.tsx
+++ src/app/components/post/Post.tsx
@@ -22,7 +22,7 @@ const Post: React.FC<PostProps> = ({ userid, updateDate, media, likes, comments,
   };
 
   return (
-    <div className="border border-gray-700 p-4 m-4 bg-gray-100 dark:bg-gray-900 text-black dark:text-white rounded-lg">
+    <div className="border border-gray-700 p-4 m-4 bg-gray-100 dark:bg-gray-900 text-black dark:text-white rounded-lg max-w-4xl mx-auto">
       <div className="flex justify-between items-center mb-4">
         <div className="flex items-center">
           <div className="text-black dark:text-white bg-white dark:bg-black rounded-full w-8 h-8 flex items-center justify-center">
@@ -35,8 +35,8 @@ const Post: React.FC<PostProps> = ({ userid, updateDate, media, likes, comments,
         </div>
       </div>
 
-      <div className="text-black dark:text-white bg-white dark:bg-black rounded-lg h-64 mb-4 flex items-center justify-center overflow-hidden">
-        <img src={media} alt="Media content" className="h-full w-full object-cover" />
+      <div className="text-black dark:text-white bg-white dark:bg-black rounded-lg mb-4 mx-auto flex items-center justify-center overflow-hidden w-fit self-center">
+        <img src={media} alt="Media content" className="h-full object-cover" />
       </div>
 
       <div className="flex justify-around mb-4">
