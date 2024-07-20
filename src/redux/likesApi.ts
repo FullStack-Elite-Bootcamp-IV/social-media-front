caa50289d237fd550ae7f8f6d79e1ed604c6891b
Miguel Cano <129132845+MiguelCano-ia@users.noreply.github.com>
2024-07-20 12:05:50 -0500

    export endpoints

--- src/redux/likesApi.ts
+++ src/redux/likesApi.ts
@@ -38,3 +38,5 @@ export const likesApi = createApi ({
     }),
   })
 })
+
+export const { useCreateLikeMutation, useGetAllLikesQuery, useDeleteLikeMutation, useGetLikesByPostIdQuery, useGetLikesByUserIdQuery } = likesApi;
