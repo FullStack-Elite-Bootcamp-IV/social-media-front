2433f5435679e93eeb96ddd6be2aa1f3dc15a43e
birulboy <brahian.castro@utp.edu.co>
2024-07-20 12:31:40 -0500

    a

--- src/redux/usersApi.ts
+++ src/redux/usersApi.ts
@@ -4,7 +4,7 @@ export const usersApi = createApi ({
   reducerPath: 'usersApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://social-media-api-1.onrender.com' }),
   endpoints: (builder) => ({ 
-    get: builder.query({
+    get : builder.query({
       query: (param) => ({
         url: `Endpoint/${param}`
       }),
