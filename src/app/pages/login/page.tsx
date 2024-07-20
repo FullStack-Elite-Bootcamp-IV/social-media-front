848db9269c795f458522ed6968201fa0a2e92339
CamiloMunozAL <camilo.munoz2@utp.edu.co>
2024-07-20 18:27:49 -0500

    Agregando loginschema y Validacion del login con zod

--- src/app/pages/login/page.tsx
+++ src/app/pages/login/page.tsx
@@ -1,79 +1,102 @@
-'use client'
+"use client";
 
-import { FormEvent, useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAuth } from "../../context/authContext";
-import Cookies from "js-cookie"
+import Cookies from "js-cookie";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { loginSchema } from "@/app/validations/loginSchema";
+import { z } from "zod";
+import Link from "next/link";
+
+
+// The type of the form inputs is inferred from the schema
+type LoginFormInputs = z.infer<typeof loginSchema>;
 
 const Login = () => {
-    const { loginToken, register, login } = useAuth();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const { loginToken, login } = useAuth();
+
+  // useForm hook with zodResolver to validate the form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormInputs>({
+    resolver: zodResolver(loginSchema),
+  });
 
 
-    useEffect(() => {
-        const token = Cookies.get("token");
-        if (token) {
-            const decodeToken = JSON.parse(token);
-            console.log(decodeToken);
-        }
-        else {
-            console.log("No hay cookie");
-        }
-
-    }, [])
-
-    const handleLogin = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        login(email, password);
+  useEffect(() => {
+    const token = Cookies.get("token");
+    if (token) {
+      const decodeToken = JSON.parse(token);
+      console.log(decodeToken);
+    } else {
+      console.log("No hay cookie");
     }
+  }, []);
 
-    return (
-      <main className="flex bg-darkVoid sm:bg-slateGray w-full min-h-screen">
-        <div className="contenedor-login bg-darkVoid w-96 rounded-3xl pt-5 pb-5 pl-5 pr-5 m-auto">
-          <h1 className="text-center  text-blancoHueso text-4xl">Nexo</h1>
-          <form
-            id="loginForm"
-            onSubmit={handleLogin}
-            className="flex flex-col  mt-4 mx-4"
+  // Function to handle the form submission and call the login function
+  const onSubmit = (data: LoginFormInputs) => {
+    login(data.email, data.password);
+  };
+
+  return (
+    <main className="flex bg-darkVoid sm:bg-slateGray w-full min-h-screen">
+      <div className="contenedor-login bg-darkVoid w-96 rounded-3xl pt-5 pb-5 pl-5 pr-5 m-auto">
+        <h1 className="text-center  text-blancoHueso text-4xl">Nexo</h1>
+        <form
+          id="loginForm"
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col mt-4 mx-4"
+        >
+          <label htmlFor="email" className="text-blancoHueso text-sm mb-1">
+            Email
+          </label>
+          <input
+            className="rounded-md bg-slateGray h-10 text-blancoHueso p-4"
+            type="email"
+            id="email"
+            placeholder="Email"
+            {...register("email")}
+          />
+          {errors.email && (
+            <span className="text-red-500 text-sm">{errors.email.message}</span>
+          )}
+          <label
+            htmlFor="password"
+            className="text-blancoHueso text-sm mb-1 mt-2"
           >
-            <label htmlFor="email" className="text-blancoHueso text-sm  mb-1">
-              Email
-            </label>
-            <input
-              className="rounded-md bg-slateGray h-10 text-blancoHueso p-4"
-              type="email"
-              id="email"
-              placeholder=" Email"
-              required
-              onChange={(e) => setEmail(e.target.value)}
-            ></input>
-            <label
-              htmlFor="password"
-              className="text-blancoHueso  text-sm  mb-1 mt-2"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              placeholder=" Password"
-              required
-              onChange={(e) => setPassword(e.target.value)}
-              className="rounded-md bg-slateGray h-10 text-blancoHueso p-4"
-            ></input>
-            <button
-              type="submit"
-              className="text-blancoHueso bg-liquidLava rounded-md mt-2 h-10"
-            >
-              Login
-            </button>
-          </form>
-          <p className=" mt-5 text-center text-xs text-blancoHueso">
-            Don’t have an account? <strong>Sign up</strong>
-          </p>
-        </div>
-      </main>
-    );
-}
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            {...register("password")}
+            className="rounded-md bg-slateGray h-10 text-blancoHueso p-4"
+          />
+          {errors.password && (
+            <span className="text-red-500 text-sm">
+              {errors.password.message}
+            </span>
+          )}
+          <button
+            type="submit"
+            className="text-blancoHueso bg-liquidLava rounded-md mt-2 h-10"
+          >
+            Login
+          </button>
+        </form>
+        <p className="mt-5 text-center text-xs text-blancoHueso">
+          Don’t have an account?
+          <Link href="/pages/register">
+            <strong>Sign up</strong>
+          </Link>
+        </p>
+      </div>
+    </main>
+  );
+};
 
-export default Login;  
+export default Login;
