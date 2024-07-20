15ed463af829352a3316e416745d07a8c0f3fce2
JuanF-Cano <juan.cano5@utp.edu.co>
2024-07-20 11:21:10 -0500

    usersApi

--- src/redux/usersApi.ts
+++ src/redux/usersApi.ts
@@ -4,9 +4,44 @@ export const usersApi = createApi ({
   reducerPath: 'usersApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://social-media-api-1.onrender.com' }),
   endpoints: (builder) => ({ 
-    get: builder.query({
-      query: (param) => ({
-        url: `Endpoint/${param}`
+    getAllUsers: builder.query({
+      query: () => ({
+        url: `/api/users/users`,
+      }),
+    }),
+
+    getUserById: builder.query({
+      query: (id) => ({
+        url: `/api/users/users/${id}`,
+      }),
+    }),
+
+    getUserByUserName: builder.query({
+      query: (userName) => ({
+        url: `/api/users/users/${userName}`,
+      }),
+    }),
+
+    register: builder.mutation({
+      query: (body) => ({
+        url: `/api/users/register`,
+        method: 'POST',
+        body,
+      }),
+    }),
+
+    editProfile: builder.mutation({
+      query: ({body, id}) => ({
+        url: `/api/users/${id}`,
+        method: 'PATCH',
+        body,
+      }),
+    }),
+
+    deleteUser: builder.mutation({
+      query: (id) => ({
+        url: `/api/users/${id}`,
+        method: 'DELETE',
       }),
     }),
   })
