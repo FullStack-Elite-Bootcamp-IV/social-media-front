16788fcc31d68b86e284df28cac2366a08d081c6
BrahianC <brahian.castro@utp.edu.co>
2024-07-20 15:30:28 -0500

    Update messagesApi.ts

--- src/redux/services/messagesApi.ts
+++ src/redux/services/messagesApi.ts
@@ -6,8 +6,42 @@ export const messagesApi = createApi ({
   endpoints: (builder) => ({ 
     get: builder.query({
       query: (param) => ({
-        url: `Endpoint/${param}`
+        url: Endpoint/${param}
+      }),
+    }),
+    messages: builder.mutation({
+      query: () => ({
+        url: /api/messages,
+        method: 'POST'
+      }),
+    }),
+
+    messagesByChatId: builder.query({
+      query: (chatId) => ({
+        url: /api/messages/${id},
+      }),
+    }),
+
+    deleteMessages: builder.mutation({
+      query: (id) => ({
+        url: /api/messages/${id},
+        method: 'DELETE',
+      }),
+    }),
+
+    
+    messagesByUserId: builder.query({
+      query: (userId) => ({
+        url: /api/messages/${userId},
       }),
     }),
   })
-})
+});
+
+
+export const {
+  messages,
+  messagesByChatId,
+  deleteMessages,
+  messagesByUserId
+} = messagesApi;
