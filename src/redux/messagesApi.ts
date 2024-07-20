b027511395b400ac0a5fe3f162ed9390a4d78ce0
Santiago3102 <santiago.gomez3@utp.edu.co>
2024-07-20 14:04:24 -0500

    Api messages

--- src/redux/messagesApi.ts
+++ src/redux/messagesApi.ts
@@ -9,5 +9,39 @@ export const messagesApi = createApi ({
         url: `Endpoint/${param}`
       }),
     }),
+    messages: builder.mutation({
+      query: () => ({
+        url: `/api/messages`,
+        method: 'POST'
+      }),
+    }),
+
+    messagesByChatId: builder.query({
+      query: (chatId) => ({
+        url: `/api/messages/${id}`,
+      }),
+    }),
+
+    deleteMessages: builder.mutation({
+      query: (id) => ({
+        url: `/api/messages/${id}`,
+        method: 'DELETE',
+      }),
+    }),
+
+    
+    messagesByUserId: builder.query({
+      query: (userId) => ({
+        url: `/api/messages/${userId}`,
+      }),
+    }),
   })
-})
+});
+
+
+export const {
+  messages,
+  messagesByChatId,
+  deleteMessages,
+  messagesByUserId
+} = messagesApi;
