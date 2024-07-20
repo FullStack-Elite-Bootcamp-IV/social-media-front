873d3d93a542f30ae3b4ab249ac30c0815e89c4e
tania-mz <zamoratania003@gmail.com>
2024-07-20 12:14:04 -0500

    Endpoints favourites api

--- src/redux/favouritesApi.ts
+++ src/redux/favouritesApi.ts
@@ -4,10 +4,31 @@ export const favouritesApi = createApi ({
   reducerPath: 'favouritesApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://social-media-api-1.onrender.com' }),
   endpoints: (builder) => ({ 
-    get: builder.query({
-      query: (param) => ({
-        url: `Endpoint/${param}`
+    getFavouritesByUserId: builder.query({
+      query: (userId) => ({
+        url: `/api/favourites/get/${userId}`,
       }),
     }),
-  })
+
+    deleteFavourite: builder.mutation({
+      query: (id) => ({
+        url: `/api/favourites/delete/${id}`,
+        method: 'DELETE',
+    }),
+  }),
+
+     addFavourite: builder.mutation({
+       query: (body) => ({
+        url: `/api/favourites/add`,
+        method: 'POST',
+        body,
+    }),
+  }), 
 })
+});
+
+export const {
+  useGetFavouritesByUserIdQuery,
+  useDeleteFavouriteMutation,
+  useAddFavouriteMutation,
+} = favouritesApi;
