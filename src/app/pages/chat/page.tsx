48f5d9322ab715c6a8fde02e11a57931626a7509
Joseph Martínez <insertepala@gmail.com>
2024-07-20 22:16:42 -0500

    Fixed responsive

--- src/app/pages/chat/page.tsx
+++ src/app/pages/chat/page.tsx
@@ -10,24 +10,29 @@ function Messages ({messages, chatId}) {
     return (
         <ul className="flex flex-col gap-2 py-2">
             {messages.filter((msg) => msg.chatId === chatId).map((msg, index) => (
-                <Message key={index} message={msg.message} />
+                <Message key={index} message={msg.message} userId={msg.userId} />
             ))}
         </ul>
     );
 }
 
-function Message ({message}) {
+function Message ({message, userId}) {
     return (
-        <li className="py-2 px-6 bg-ligthPurple rounded-xl w-fit text-blancoHueso">{message}</li>
+        <li
+            className={`py-2 px-6 ${
+                userId === localStorage.getItem('loginToken')
+                ? 'bg-ligthPurple self-end'
+                : 'bg-liquidLava'
+            } rounded-xl w-fit max-w-full text-blancoHueso`}
+            >
+            <p className="w-full text-wrap break-words">{message}</p>
+        </li>
     );
 }
 
-const socket = io('https://8c81mq74-5002.use2.devtunnels.ms/chat', {
-            auth: {
-                token: 234,
-            }});
+let socket;
 
-const Chat = () => {
+function Chat() {
     const chatId = useSearchParams().get('chatId');
     const { loginToken, register } = useAuth();
     const [messages, setMessages ] = useState([]);
@@ -36,28 +41,37 @@ const Chat = () => {
     useEffect(() => {
         if (typeof window === undefined) return 
 
+        
+        socket = io('https://8c81mq74-5002.use2.devtunnels.ms/chat', {
+            auth: {
+                token: localStorage.getItem('loginToken'),
+            }});
+
         socket.on('connect', () => {
             console.log('Connected to server');
         });
 
 
-        socket.on('receiveMessage', ({message, chatId}) => {
-            console.log(message);
+        socket.on('receiveMessage', ({message, chatId, userId}) => {
             if (message) { 
-                setMessages(prevMessages => [...prevMessages, {message, chatId}]);
+                setMessages(prevMessages => [...prevMessages, {message, chatId, userId}]);
             }
         })
-        console.log('chatId', chatId);
+
+        return () => {
+            socket.disconnect();
+        }
     }, []);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const message = e.target.message.value
-        socket.emit('message', message, chatId);
+        if (socket) socket.emit('message', message, chatId);
+        e.target.message.value = '';
     }
     
     return (
-        <main className="flex justify-center items-end w-dvw h-dvh bg-purple-50 dark:bg-darkVoid py-4 ">
+        <main className="flex justify-center items-end w-dvw h-dvh bg-purple-50 dark:bg-darkVoid py-4">
             <div className="w-full max-w-2xl px-4 md:px-0">
                 <Messages messages={messages} chatId={chatId}/>
                 <form onSubmit={handleSubmit} className="flex">
