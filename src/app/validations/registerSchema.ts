4786c1ba9bf5a40f71ecd42057cdf4fc1067a907
Miguel Cano <129132845+MiguelCano-ia@users.noreply.github.com>
2024-07-20 19:00:09 -0500

    register scheme changed

--- src/app/validations/registerSchema.ts
+++ src/app/validations/registerSchema.ts
@@ -9,10 +9,10 @@ export const registerSchema = z.object({
   email: z.string().email({
     message: 'Invalid email address',
   }),
-  password: z.string().min(6, {
+  password: z.string().min(8, {
     message: 'Password must be at least 6 characters long',
   }),
-  confirmPassword: z.string().min(6, {
+  confirmPassword: z.string().min(8, {
     message: 'Password must be at least 6 characters long',
   }),
 }).refine(data => data.password === data.confirmPassword, {
