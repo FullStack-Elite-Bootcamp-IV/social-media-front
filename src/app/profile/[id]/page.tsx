198a3b38e9f254074c67b886f8918d4c91fe212a
juank3690 <juan.gomez5@utp.edu.co>
2024-07-21 00:14:01 -0500

    pantalla

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -226,7 +226,7 @@ const Profile = () => {
           </div>
         </section>
 
-        <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 gap-4">
+        <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {posts.map((post:any , index:any) => (
             <Post
               key={index}
