e2a6fd40caeb8414e6f8571ae4b02648dffb74a1
JuanF-Cano <juan.cano5@utp.edu.co>
2024-07-22 08:20:48 -0500

    Arreglo para windows. Local

--- src/components/notification/NotificationCard.tsx
+++ src/components/notification/NotificationCard.tsx
@@ -3,30 +3,29 @@ import React from "react";
 import { IoClose } from "react-icons/io5";
 import { format } from "date-fns";
 
-// Interface for the NotificationCard component
 interface NotificationCardProps {
-  notificationId: string;
-  emisorName: string;
-  message: string;
-  date: string;
-  onDelete: (notificationId: string) => void; // Function to delete a notification
+  id: string; 
+  emisorUser: string; 
+  title: string;
+  notificationDate: string;
+  onDelete: (id: string) => void; 
 }
 
 // Component to display a notification card
 const NotificationCard: React.FC<NotificationCardProps> = (props) => {
-  const formattedDate = format(new Date(props.date), "dd MMM yyyy, HH:mm"); // Format the date using date-fns library
+  const formattedDate = format(new Date(props.notificationDate), "dd MMM yyyy, HH:mm"); // Format the date using date-fns library
   return (
     <div className="md:max-w-64 w-full bg-ligthPurple rounded-lg p-4 mb-4 text-blancoHueso">
       <div className="flex justify-between items-center">
         <div>
           <p className="font-bold">
-            {props.emisorName} {props.message}
+            {props.emisorUser} - {props.title}
           </p>
         </div>
         <button
           className="text-gray-500 hover:text-gray-700"
           aria-label="Close Notification"
-          onClick={() => props.onDelete(props.notificationId)} // Call the onDelete function when the button is clicked
+          onClick={() => props.onDelete(props.id)}
         >
           <IoClose className="text-xl" />
         </button>
