2df1898ff4549b1e7d4e4dbba6d0cce4fa146afd
David London <dlondon.dev@gmail.com>
2024-07-22 10:52:00 -0500

    dark mode fixed on profile

--- src/app/profile/[userName]/page.tsx
+++ src/app/profile/[userName]/page.tsx
@@ -195,7 +195,7 @@ const Profile = ({ params: userName }: { params: { userName: string } }) => {
   const [posts, setPosts] = useState<PostData[]>(postsArray);
 
   return (
-    <div className="min-h-screen bg-gray-900">
+    <div className="min-h-screen bg-blancoHueso dark:bg-gray-900">
       <Navbar />
       <main className="flex md:ml-64 min-h-screen">
         <div className="w-full px-4 md:px-8 lg:px-16 mt-10 md:mt-0">
@@ -206,7 +206,7 @@ const Profile = ({ params: userName }: { params: { userName: string } }) => {
               height: "200px",
             }}
           >
-            <div className="absolute inset-0 flex justify-center items-center bg-black bg-opacity-50">
+            <div className="absolute inset-0 flex justify-center items-center dark:bg-black bg-opacity-50">
               <img
                 className="rounded-full border-4 border-white w-24 h-24 sm:w-32 sm:h-32 md:w-40 md:h-40 lg:w-48 lg:h-48"
                 src={datos[id].imagePerfil}
@@ -215,14 +215,14 @@ const Profile = ({ params: userName }: { params: { userName: string } }) => {
             </div>
           </section>
 
-          <div className="text-center text-white mb-8">
+          <div className="text-center text-darkVoid dark: dark:text-blancoHueso mb-8">
             <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold">
               {datos[id].username}
             </h1>
             <div className="flex flex-col sm:flex-row sm:justify-center sm:space-x-4 mt-4 text-base sm:text-lg">
               <div className="flex flex-col items-center mb-4 sm:mb-0">
                 <p className="text-xl">{datos[id].post}</p>
-                <p className="text-gray-400">posts</p>
+                <p className="text-darkVoid dark:text-gray-400">posts</p>
               </div>
               <div className="flex flex-col items-center mb-4 sm:mb-0">
                 <p className="text-xl">{datos[id].followers}</p>
@@ -253,23 +253,23 @@ const Profile = ({ params: userName }: { params: { userName: string } }) => {
           <section className="text-white mb-8">
             <div className="flex flex-col sm:flex-row sm:justify-around text-base sm:text-lg">
               <div className="mb-4 sm:mb-0">
-                <p className="font-semibold text-gray-400">Nombre</p>
-                <p>{datos[id].name}</p>
+                <p className="font-semibold text-darkVoid dark:text-gray-400">Nombre</p>
+                <p className="text-darkVoid dark:text-white">{datos[id].name}</p>
               </div>
               <div className="mb-4 sm:mb-0">
-                <p className="font-semibold text-gray-400">Edad</p>
-                <p>{datos[id].age}</p>
+                <p className="font-semibold text-darkVoid dark:text-gray-400">Edad</p>
+                <p className="text-darkVoid dark:text-white">{datos[id].age}</p>
               </div>
               <div className="mb-4 sm:mb-0">
-                <p className="font-semibold text-gray-400">Género</p>
-                <p>{datos[id].genre}</p>
+                <p className="font-semibold text-darkVoid dark:text-gray-400">Género</p>
+                <p className="text-darkVoid dark:text-white">{datos[id].genre}</p>
               </div>
             </div>  
           </section>
 
           <section className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-6">
             {posts.map((post, index) => (
-              <div key={index} className="bg-gray-800 rounded-lg shadow-lg p-4">
+              <div key={index} className="bg-gray-200 dark:bg-gray-800 rounded-lg shadow-lg p-4">
                 <Post
                   userid={post.userid}
                   title={post.title}
