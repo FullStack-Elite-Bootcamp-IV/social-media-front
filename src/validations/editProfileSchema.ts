73f9acbbb1f56db7bd4d6593eacdda3a5af239e9
AGBR121 <angel.burbano@utp.edu.co>
2024-07-30 20:36:32 -0500

    editprofileschema update

--- src/validations/editProfileSchema.ts
+++ src/validations/editProfileSchema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 const urlComprobation = /^(ftp|http|https):\/\/[^ "]+$/; // Comprueba que el enlace ingresado por el usuario sea un URL válido
 
 export const editProfileSchema = z.object({
-    fullname: z.string().min(5, { message: 'Fullname must be at least 5 characters long' }).optional(),
+    fullName: z.string().min(5, { message: 'Fullname must be at least 5 characters long' }).optional(),
     description: z.string().max(180, { message: 'Description must be shorter than 180 characters' }).optional(),
     gender: z.string().optional(),
     location: z.string().optional(),
