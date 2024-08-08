d4fadb7b8a9904c99812b4bcf874b32fb3c5a1a8
Joseph Martinez <insertepala@gmail.com>
2024-08-08 14:31:49 -0500

    Up to date to main

--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -1,54 +1,71 @@
-import { useState, useEffect } from "react";
+'use client';
+
+import { useEffect, useState } from "react";
 import {
   useGetCommentsByPostIdQuery,
   useAddComentMutation,
   useUpdateCommentMutation,
   useDeleteCommentMutation,
 } from "@/store/services/commentsApi";
-import CommentIcon from "@/icons/CommentIcon";
 import Comment from "./Comment";
-import HeartIcon from "@/icons/HeartIcon";
-import UserCircleIcon from "@/icons/UserCircleIcon";
-import {jwtDecode} from "jwt-decode";
+import { useUser } from "@/context/UserContext";
+import { useCreateNotificationMutation } from "@/store/services/notificationsApi";
 
-interface MyJwtPayload {
-  id: string;
+interface CommentsProps {
+  userId: string;
+  postId: string;
+  setShowComments: Function;
 }
 
-const Comments = ({ postId }: { postId: string }) => {
-  const [newComment, setNewComment] = useState("");
-  const [userId, setUserId] = useState<string | null>(null);
+interface Comment {
+  id: string;
+  userId: string;
+  content: string;
+  createdAt: Date;
+}
 
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      try {
-        const decodedToken = jwtDecode<MyJwtPayload>(token);
-        setUserId(decodedToken.id);
-      } catch (error) {
-        console.error("Failed to decode token", error);
-      }
-    }
-  }, []);
-
-  const { data: comments, error, isLoading, isError } = useGetCommentsByPostIdQuery(postId);
+const Comments = ({ userId, postId, setShowComments }: CommentsProps) => {
   const [addComment] = useAddComentMutation();
   const [updateComment] = useUpdateCommentMutation();
   const [deleteComment] = useDeleteCommentMutation();
+  const [createNotification] = useCreateNotificationMutation();
+
+  const [newComment, setNewComment] = useState("");
+  const [comments, setComments] = useState([]);
+  const { data: commentsData, isLoading, isSuccess } = useGetCommentsByPostIdQuery(postId);
+  const { user } = useUser();
+
+  const notificationContent = `${user?.userName} Has commented in your post`;
+
+  useEffect(() => {
+    if (isSuccess && commentsData) {
+      const sortedComments = commentsData?.slice().sort((a: Comment, b: Comment) => {
+        const dateA = new Date(a.createdAt);
+        const dateB = new Date(b.createdAt);
+
+        return dateB.getTime() - dateA.getTime();
+      });
+
+      setComments(sortedComments);
+    }
+  }, [isSuccess, commentsData]);
 
   const handleAddComment = async () => {
     try {
-      if (!userId) {
-        throw new Error("User not authenticated");
-      }
-
-      await addComment({
-        postId,
-        body: {
-          userId,
-          content: newComment,
-        },
-      }).unwrap();
+      const myComment = await addComment({
+        userId: user?.userId,
+        postId: postId,
+        content: newComment,
+      });
+      const notification = await createNotification({
+        emisorUser: user?.userId,
+        receptorUser: userId,
+        action: 'comments',
+        title: "New Comment",
+        description: notificationContent
+      });
+      console.log(notification)
+      console.log("Comment info:", myComment);
       setNewComment("");
     } catch (error) {
       console.error("Failed to add comment", error);
@@ -57,7 +74,7 @@ const Comments = ({ postId }: { postId: string }) => {
 
   const handleDeleteComment = async (commentId: string) => {
     try {
-      await deleteComment({ postId, commentId }).unwrap();
+      await deleteComment(commentId);
     } catch (error) {
       console.error("Error deleting comment:", error);
     }
@@ -66,10 +83,12 @@ const Comments = ({ postId }: { postId: string }) => {
   const handleEditComment = async (commentId: string, updatedContent: string) => {
     try {
       await updateComment({
-        postId,
-        commentId,
-        body: { content: updatedContent },
-      }).unwrap();
+        commentId: commentId,
+        body: {
+          content: updatedContent,
+          createdAt: new Date()
+        },
+      });
     } catch (error) {
       console.error("Error editing comment:", error);
     }
@@ -79,64 +98,42 @@ const Comments = ({ postId }: { postId: string }) => {
     return <div>Loading...</div>;
   }
 
-  // if (isError) {
-  //   console.error("Error loading comments:", error);
-  //   return <div>Error loading comments</div>;
-  // }
+  const handleClose = () => {
+    setShowComments(false);
+  };
 
   return (
-    <section className="flex items-center justify-center py-10">
-      <div className="bg-gray-70 text-darkVoid dark:bg-darkVoid dark:text-white w-full flex flex-col md:flex-row">
-        <div className="w-full flex flex-col">
-          <div>
-            <div className="flex p-2 items-center mb-4">
-              <div className="mr-2 cursor-pointer">
-                <UserCircleIcon />
-              </div>
-              <div className="flex-grow">
-                <div className="font-bold">User</div>
-                <div className="">Description</div>
-              </div>
-            </div>
-            <div className="w-full h-[1px] bg-gray-70" />
-            <div className="space-y-2 p-2">
-              {comments && comments.length > 0 ? (
-                comments.map((comment: { id: string, userId: string, content: string, date: string }) => (
-                  <Comment
-                    key={comment.id}
-                    comment={comment}
-                    onDelete={() => handleDeleteComment(comment.id)}
-                    onEdit={(updatedContent: string) => handleEditComment(comment.id, updatedContent)}
-                  />
-                ))
-              ) : (
-                <div>No comments yet. Be the first to comment!</div>
-              )}
-            </div>
+    <section className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-70">
+      <div className="bg-gray-70 w-1/2 max-h-5/6 ml-60 rounded-2xl p-4 text-darkVoid dark:bg-zinc-950 dark:text-white flex flex-col">
+        <div className="w-full flex flex-col overflow-hidden">
+          <div className="flex justify-end mb-2">
+            <button className="text-white text-xl" onClick={handleClose}>&times;</button>
           </div>
-
-          <div>
-            <div className="w-full bg-blancoHueso" />
-            <div className="flex items-center p-2 gap-6">
-              <div className="flex items-center cursor-pointer">
-                <HeartIcon />
-                <span className="ml-1 cursor-default">20</span>
+          <div className="overflow-y-auto scrollbar-hidden p-2">
+            {comments && comments.length > 0 ?
+              comments?.map((comment: Comment) => (
+                <Comment
+                  key={comment.id}
+                  comment={comment}
+                  onDelete={() => handleDeleteComment(comment.id)}
+                  onEdit={(updatedContent: string) => handleEditComment(comment.id, updatedContent)}
+                />
+              ))
+             :
+              <div className="w-full flex items-center justify-center">
+                There are no comments yet. Add one
               </div>
-              <div className="flex mb-1 cursor-pointer">
-                <CommentIcon />
-              </div>
-            </div>
-            <div className="w-full bg-blancoHueso" />
-            <div className="p-3">
-              <input
-                type="text"
-                placeholder="Add a comment..."
-                className="w-full bg-slateGray p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-ligthPurple"
-                value={newComment}
-                onChange={(e) => setNewComment(e.target.value)}
-              />
-              <button onClick={handleAddComment} className="mt-2 bg-liquidLava text-white p-2 rounded">Add Comment</button>
-            </div>
+            }
+          </div>
+          <div className="w-full flex items-center justify-center p-3 gap-6 mt-2">
+            <input
+              type="text"
+              placeholder="Add a comment..."
+              className="w-4/5 bg-slateGray p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-ligthPurple"
+              value={newComment}
+              onChange={(e) => setNewComment(e.target.value)}
+            />
+            <button onClick={handleAddComment} className="bg-liquidLava text-white py-2 px-4 rounded">Send</button>
           </div>
         </div>
       </div>
